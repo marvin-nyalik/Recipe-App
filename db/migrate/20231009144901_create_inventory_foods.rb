77f71c503cf5e63617208462353ac020590eb1ed
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-10-09 21:38:43 +0600

    Create Models for Inventory Food and Recipe Food

--- db/migrate/20231009144901_create_inventory_foods.rb
+++ db/migrate/20231009144901_create_inventory_foods.rb
@@ -1,7 +1,10 @@
 class CreateInventoryFoods < ActiveRecord::Migration[7.1]
   def change
     create_table :inventory_foods do |t|
-
+      t.integer :quantity, default: 0
+      t.references :food, null: false, foreign_key: true
+      t.references :inventory, null: false, foreign_key: true
+      
       t.timestamps
     end
   end
