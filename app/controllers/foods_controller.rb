819b6237e11fb1e357f41573fee6dbed2ccf23a3
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-10-10 13:22:19 +0600

    Fix linter errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,14 +3,13 @@ class FoodsController < ApplicationController
     @food = Food.new(food_params)
 
     if @food.save
-      redirect_to recipes_path, notice:'Food successfully added'
+      redirect_to recipes_path, notice: 'Food successfully added'
     else
-      render :new, notice:"Failed"
+      render :new, notice: 'Failed'
     end
   end
 
-  def new
-  end
+  def new; end
 
   def destroy
     @food = Food.find(params[:id])
@@ -18,8 +17,8 @@ class FoodsController < ApplicationController
     # redirect_to foods_path, notice: 'Food deleted successfully'
   end
 
-  private 
-  
+  private
+
   def food_params
     params.permit(:name, :measurement_unit, :price)
   end
